2c34f8ec027dd7d62a51f5f3213cd8a10717530d
rgcienfuegos <rgcienfuegos@hotmail.com>
2020-07-25 16:20:53 +0200

    footer

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,51 @@
-function buildScreen(htmlString) {
-    let div = document.createElement("div");
-    div.innerHTML = htmlString;
-    return div.children[0]
-  }
+const divResult = document.getElementById("divResult");   
+window.onload=getRepos();
+
+        function clear(){
+         while(divResult.firstChild) 
+            divResult.removeChild(divResult.firstChild)
+        }
+
+        async function getRepos() {
+             clear();
+            const url = "https://api.github.com/users/rgcienfuegos/repos"
+            const response = await fetch(url)
+            const result = await response.json()
+
+         
+
+
+            let counter = 0;
+
+            result.forEach(i=>{                  
+              
+              counter=counter+1;
+
+              let  repoHTML =`<h2 id='nameRepo`+counter+`'></h2>
+                          <p id='desRepo`+counter+`'></p>
+                          <p><a id='butRepoDet`+counter+`' class="btn btn-secondary" href="#" role="button">Mas info&raquo;</a>
+                              <a id='butRepoDem`+counter+`' class="btn btn-secondary" href="https://rgcienfuegos.github.io/`+i.name+`//" role="button">Link demo &raquo;</a>
+                              </p>
+                            `
+
+
+
+                        let div = document.createElement("div");
+                      
+                        div.innerHTML = repoHTML;
+                        div.className="col-md-4";
+                  
+                        divResult.appendChild(div)    
+                        let nameRepo = document.getElementById("nameRepo"+counter);  
+                        nameRepo.href = i.html_url;
+                        nameRepo.textContent = i.name;  
+                        let desRepo = document.getElementById("desRepo"+counter);  
+                        desRepo.textContent = i.description;
+
+                          
+                     
+                      })
+
+}
+
+    
