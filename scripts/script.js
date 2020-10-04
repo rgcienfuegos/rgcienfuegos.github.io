90ed4f01abd204adcf9f8a67232aef040ded9ca5
rgcienfuegos <rgcienfuegos@hotmail.com>
2020-10-04 15:51:27 +0200

    v2

--- scripts/script.js
+++ scripts/script.js
@@ -3,57 +3,48 @@
             divResult.removeChild(divResult.firstChild)
         }
 
-        async function getRepos() {
-            
+        async function getRepos() {            
              try {
               const url = "https://api.github.com/users/rgcienfuegos/repos"
               const response = await fetch(url)
               const result = await response.json()
   
-             } catch (error) {
-
-               
-             }
-             
+             } catch (error) {               
+             }             
             return result;
             }    
          
 
-            function getElements(result) {
-            let counter = 0;
+        function getElements(result) {
+        let counter = 0;
 
-            result.forEach(i=>{                  
+        result.forEach(i=>{                  
               
-              counter=counter+1;
+        counter=counter+1;
 
-              let  repoHTML =`
-              <h2 id='nameRepo`+counter+`'></h2>
-              <p id='desRepo`+counter+`'></p>
-              <p>
-                <a id='butRepoDet`+counter+`' class="btn btn-secondary" href="#" role="button">Mas info&raquo;</a>
-                <a id='butRepoDem`+counter+`' class="btn btn-secondary" href="https://rgcienfuegos.github.io/`+i.name+`//" role="button">Link demo &raquo;</a>
-              </p>
-              `        
-                        let div = document.createElement("div");
-                      
-                        div.innerHTML = repoHTML;
-                        div.className="col-md-4";
+        let  repoHTML =`
+        <h2 id='nameRepo`+counter+`'></h2>
+        <p id='desRepo`+counter+`'></p>
+        <p>
+          <a id='butRepoDem`+counter+`' class="btn btn-secondary" href="https://rgcienfuegos.github.io/`+i.name+`//" role="button">Link demo &raquo;</a>
+        </p>
+        `      
+        let div = document.createElement("div");                      
+        div.innerHTML = repoHTML;
+        div.className="col-md-4";                  
+        divResult.appendChild(div)    
+        let nameRepo = document.getElementById("nameRepo"+counter);  
+        nameRepo.href = i.html_url;
+        nameRepo.textContent = i.name;  
+        let desRepo = document.getElementById("desRepo"+counter);  
+        desRepo.textContent = i.description;           
                   
-                        divResult.appendChild(div)    
-                        let nameRepo = document.getElementById("nameRepo"+counter);  
-                        nameRepo.href = i.html_url;
-                        nameRepo.textContent = i.name;  
-                        let desRepo = document.getElementById("desRepo"+counter);  
-                        desRepo.textContent = i.description;
-
-                          
-                     
                       })
 
-}
+        }
 
-
-
+        // Script
+      
 const divResult = document.getElementById("divResult");   
 window.onload=getRepos();
 clear();
